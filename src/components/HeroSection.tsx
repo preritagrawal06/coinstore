48c8b017aa6bd5b9d2d48fa185a201a71a53f9ce
SamSoham <ssoham5700@gmail.com>
2024-09-04 23:17:10 +0530

    Hero Section completed

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,32 @@
+import Button from "./Button";
+
+export default function HeroSection(){
+    return(
+        <div className="bgScreen">
+            <div className="bgSvg">
+                <svg xmlns="http://www.w3.org/2000/svg" style={{width:'100%',height:'100%'}} viewBox="0 0 1010 554" fill="none" >
+                    <path d="M1008 47V507C1008 531.853 987.853 552 963 552H850.033C844.319 552 838.658 550.912 833.352 548.794L738.63 510.991C732.853 508.685 726.688 507.5 720.467 507.5H530H336.84C331.769 507.5 326.729 508.287 321.9 509.833L196.881 549.857C192.446 551.277 187.817 552 183.16 552H47C22.1472 552 2 531.853 2 507V47C2 22.1472 22.1472 2 47 2H179.262C186.336 2 193.309 3.6674 199.618 6.86693L318.573 67.2004C325.442 70.6844 333.036 72.5 340.738 72.5H536.562H715.888C725.158 72.5 734.239 69.8701 742.074 64.9157L830.563 8.96517C837.759 4.41522 846.099 2 854.612 2H963C987.853 2 1008 22.1472 1008 47Z" stroke="#009492" stroke-width="4" />
+                </svg>
+            </div>
+           <div className="flex z-10 items-center ">
+           <div className='heroImg'>
+                <img src="/hero1.png" alt="heroIcon2"/>
+                </div>
+           <div className="flex flex-col items-center">
+            <div className="text-center">
+                <p className="subtext">#No.1 website  for Game Pass</p>
+                <p className="head1">Play More, Pay Less:</p>
+                <p className="head1">Your Ultimate <span className="text-[#24FFF2]">Game Pass</span> Destination!</p>
+            </div>
+            <div className="z-10">
+               <Button name='Explore More'/>
+               <Button name='Browse Games'/>
+            </div>
+            </div>
+                <div className='heroImg'>
+                <img src="/hero2.png" alt="heroIcon2"/>
+                </div>
+           </div>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/bgImg.jpg'),linear-gradient(90deg, rgba(9, 7, 10, 0.76) 0.26%, rgba(30, 45, 74, 0.76) 99.8%)"
+      },
+      fontFamily:{
+        PostJb: 'Post No Bills Jaffna Bold',
+        PostR: 'Jaffna Regular',
+        PostJM:'Post No Bills Jaffna Medium Regular',
+        PostSB:'Post No Bills Jaffna SemiBold Regular',
+        PostEB:'Post No Bills Jaffna ExtraBold Regular'
       }
     },
   },
