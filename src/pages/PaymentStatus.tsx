2344932f4c23f6e3311da31546ed20a2a8a0b01d
preritagrawal06 <agrawalprerit780@gmail.com>
2024-10-11 01:59:57 +0530

    redirect issue fix

--- src/pages/PaymentStatus.tsx
+++ src/pages/PaymentStatus.tsx
@@ -5,14 +5,16 @@ const PaymentStatus = () => {
     const [loading, setLoading] = useState(false)
     const queryString = new URLSearchParams(window.location.search)
     const orderId = queryString.get('orderId')
-
+    const [data, setData] = useState()
     useEffect(()=>{
 
         async function getStatus(){
             try {
                 setLoading(true)
-                const {data} = await axios.get(`https://coinstore-backend.onrender.com/api/payment/payment-status?orderId:${orderId}`)
-                console.log(data);
+                const {data} = await axios.get(`https://coinstore-backend.onrender.com/api/payment/payment-status?orderId=${orderId}`)
+                setData(data);
+                console.log(data.data);
+                
             } catch (error) {
                 console.log(error);
             } finally{
@@ -27,9 +29,10 @@ const PaymentStatus = () => {
         <div className=" px-10 md:px-20 pt-40 flex flex-col gap-8 min-h-screen">
             <div className="border flex flex-col items-center justify-center">
                 <p className="text-white text-xl">Payment Status</p>
-                {
-                    loading? "Getting status": "Status updated"
+                <p className="text-white">{
+                    loading ? "Getting status": `Status: ${data ? data["data"]["status"] : "updating"}`
                 }
+                </p>
             </div>
         </div>
     </> 
