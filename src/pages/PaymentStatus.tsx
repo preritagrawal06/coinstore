c3a807c767137a9aac5911c6b4ff0c2fd57f9715
SamSoham <ssoham5700@gmail.com>
2024-10-12 21:37:25 +0530

    Payment status page modified

--- src/pages/PaymentStatus.tsx
+++ src/pages/PaymentStatus.tsx
@@ -6,6 +6,7 @@ const PaymentStatus = () => {
     const queryString = new URLSearchParams(window.location.search)
     const orderId = queryString.get('orderId')
     const [data, setData] = useState()
+    const [success,setSuccess] = useState(false)
     useEffect(()=>{
 
         async function getStatus(){
@@ -13,10 +14,10 @@ const PaymentStatus = () => {
                 setLoading(true)
                 const {data} = await axios.get(`https://coinstore-backend.onrender.com/api/payment/payment-status?orderId=${orderId}`)
                 setData(data);
-                console.log(data?.data);
-                
+                setSuccess(true)   
             } catch (error) {
                 console.log(error);
+                setSuccess(false)
             } finally{
                 setLoading(false)
             }
@@ -26,13 +27,23 @@ const PaymentStatus = () => {
     },[])
     return (
     <>
-        <div className=" px-10 md:px-20 pt-40 flex flex-col gap-8 min-h-screen">
-            <div className="border flex flex-col items-center justify-center">
-                <p className="text-white text-xl">Payment Status</p>
-                <p className="text-white">{
-                    loading ? "Getting status": `Status: ${data ? data["data"]["status"] : "updating"}`
+        <div className=" px-10 md:px-20 pt-40 flex flex-col gap-8 min-h-screen justify-center items-center">
+            <div className="flex flex-col items-center justify-center border-2 border-black dark:border-white text-center w-full md:w-[80%] lg:w-[50%]" >
+                <p className="text-black dark:text-white text-xl">Payment Status</p>
+                <p className="text-black dark:text-white ">{
+                    data && loading ? "Getting status": `Status: ${data ? data["data"]["status"] : "updating"}`
                 }
                 </p>
+                {
+                    success? 
+                    <div>
+                        <p className="text-[20px] font-PostSB">Your Transaction will be completed soon</p>
+                        <p className="">Don't leave this page</p>
+                    </div>: 
+                    <div>
+                         <p className="text-[20px] font-PostSB">Order ID Not found</p>
+                    </div>
+                }
             </div>
         </div>
     </> 
