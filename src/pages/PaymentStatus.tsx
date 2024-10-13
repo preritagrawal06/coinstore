2ad36192d8ed31df6283a09a62a271649f0b8448
preritagrawal06 <agrawalprerit780@gmail.com>
2024-10-13 18:46:35 +0530

    added topup reseller api

--- src/pages/PaymentStatus.tsx
+++ src/pages/PaymentStatus.tsx
@@ -5,16 +5,38 @@ const PaymentStatus = () => {
     const [loading, setLoading] = useState(false)
     const queryString = new URLSearchParams(window.location.search)
     const orderId = queryString.get('orderId')
-    const [data, setData] = useState()
+    const [status, setStatus] = useState()
     const [success,setSuccess] = useState(false)
     useEffect(()=>{
-
         async function getStatus(){
             try {
                 setLoading(true)
                 const {data} = await axios.get(`https://coinstore-backend.onrender.com/api/payment/payment-status?orderId=${orderId}`)
-                setData(data);
-                setSuccess(true)   
+                console.log(data);
+                if(data.success){
+                    setStatus(data);
+                    setSuccess(true)
+                    const orderDetails = orderId?.split("_")
+                    const userId = orderDetails![0]
+                    const game = orderDetails![1]
+                    const denom  = orderDetails![2]
+                    const transactionTime = orderDetails![3]
+                    const serverId = orderDetails![4]
+
+                    try {
+                        const {data: txnData} = await axios.post('https://coinstore-backend.onrender.com/api/topup/create-topup-order',{
+                            userid: userId,
+                            game,
+                            serverid: serverId,
+                            itemName: denom,
+                            transactionTime
+                        })
+                        console.log(txnData);
+                        
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
             } catch (error) {
                 console.log(error);
                 setSuccess(false)
@@ -25,13 +47,14 @@ const PaymentStatus = () => {
 
         getStatus()
     },[])
+    
     return (
     <>
         <div className=" px-10 md:px-20 pt-40 flex flex-col gap-8 min-h-screen justify-center items-center">
             <div className="flex flex-col items-center justify-center border-2 border-black dark:border-white text-center w-full md:w-[80%] lg:w-[50%]" >
                 <p className="text-black dark:text-white text-xl">Payment Status</p>
                 <p className="text-black dark:text-white ">{
-                    data && loading ? "Getting status": `Status: ${data ? data["data"]["status"] : "updating"}`
+                    status && loading ? "Getting status": `Status: ${status ? status["data"]["status"] : "OrderID not found"}`
                 }
                 </p>
                 {
