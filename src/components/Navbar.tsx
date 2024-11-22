2ffbdf14eea23854f66ba195b8dff1aa7e057cd0
SamSoham <ssoham5700@gmail.com>
2024-11-22 22:50:48 +0530

    Table updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,6 @@ export default function Navbar() {
           authorization: `Bearer ${token}`
         }
       })
-      console.log(res)
       setData(res.data.transaction)
     } catch (err: any) {
       toast({
@@ -233,6 +232,40 @@ export default function Navbar() {
                               <Input id="amount" className="col-span-3" onChange={(e) => { setAmount(e.target.value) }} />
                             </div>
                             <Button onClick={handleAddMoney}>Proceed</Button>
+                            <Table className="w-full h-full overflow-x-scroll border border-gray-200 rounded-lg shadow ">
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead className="w-12" />
+                          <TableHead className="w-[100px]">Name</TableHead>
+                          <TableHead>Email</TableHead>
+                          <TableHead>Phone Number</TableHead>
+                          <TableHead>Game Name</TableHead>
+                          <TableHead>Item Name</TableHead>
+                          <TableHead>Transaction Date</TableHead>
+                          <TableHead>Order ID</TableHead>
+                          <TableHead>User ID</TableHead>
+                          <TableHead>Payment Status</TableHead>
+                          <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {data?.map((invoice: any, index: any) => (
+                          <TableRow key={index}>
+                            <TableCell>{index + 1}</TableCell>
+                            <TableCell className="font-medium">{invoice['customerName']}</TableCell>
+                            <TableCell>{invoice['customerEmail']}</TableCell>
+                            <TableCell>{invoice['customerPhone']}</TableCell>
+                            <TableCell>{invoice['game']}</TableCell>
+                            <TableCell>{invoice['itemName']}</TableCell>
+                            <TableCell>{new Date(invoice['transactionDate']).toLocaleDateString()}</TableCell>
+                            <TableCell>{invoice['orderid']}</TableCell>
+                            <TableCell>{invoice['userid']}</TableCell>
+                            <TableCell>{invoice['paymentStatus']}</TableCell>
+                            <TableCell className="text-right">{invoice['amount']}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
                           </div>
 
                         </DialogDescription>
