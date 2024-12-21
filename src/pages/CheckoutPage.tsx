8f1a22a8ac1f9828270d36e81d6881f5e82f5028
preritagrawal06 <agrawalprerit780@gmail.com>
2024-12-21 11:25:01 +0530

    redirect URL changed

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -116,7 +116,7 @@ export default function CheckoutPage() {
             // }
             if(data.success && data.user){
                 localStorage.setItem('user', JSON.stringify(data.user))
-                window.location.href = `http://localhost:5173/status?orderId=${data.txn.orderid}&mode=wallet`
+                window.location.href = `https://shadowcompany.com/status?orderId=${data.txn.orderid}&mode=wallet`
             }
             toast({
                 description: data.message
