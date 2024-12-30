d4ea5b3f87884185a7e4e944cae536a287c45aef
preritagrawal06 <agrawalprerit780@gmail.com>
2024-12-30 22:12:25 +0530

    purchase history order change

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -20,11 +20,11 @@ export default function ContactUs(){
         <h2 className="text-3xl font-bold sm:text-4xl">Get in Touch</h2>
 
         <p className="mt-4 text-gray font-PostSB">
-        Thank you for visiting Shadow Games! Whether you’re here to ask about game passes, report a bug, or share feedback, we’d love to hear from you. Our team is dedicated to providing you with the best gaming experience and customer support.
+        Thank you for visiting Shadow Games! Whether you're here to ask about game passes, report a bug, or share feedback, we’d love to hear from you. Our team is dedicated to providing you with the best gaming experience and customer support.
         </p>
 
        <div className="mt-3 flex flex-col gap-3">
-        <p className="font-PostSB text-[20px] flex flex-row gap-1"><Mail/> Email: shadowcompany@gmail.com</p>
+        <p className="font-PostSB text-[20px] flex flex-row gap-1"><Mail/> Email: mail.shadowcompany@gmail.com</p>
         <p className="font-PostSB text-[20px] flex flex-row gap-1"><PhoneCall/> WhatsApp & Call: +91 72173 20257</p>
         <p className="text-[24px]">Social Links</p>
         <div className="flex flex-row gap-4">
