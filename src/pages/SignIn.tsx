dca45245902ec116dc83e5b1926428542bcce3f4
preritagrawal06 <agrawalprerit780@gmail.com>
2025-01-03 21:16:28 +0530

    OTP auth added

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,51 +1,75 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
 import { REGEXP_ONLY_DIGITS } from "input-otp";
-
+import validatePhoneNumber from '../utils/validatePhone'
 
 export default function SignIn() {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [credential, setCredential] = useState('')
+  // const [password, setPassword] = useState('')
   const [open, setOpen] = useState(false)
   const [value, setValue] = useState("")
   const navigate = useNavigate()
   const { toast } = useToast()
   const queryString = new URLSearchParams(window.location.search)
   const redirectURL = queryString.get('redirect')
+
   async function validateUser(e: any) {
     e.preventDefault()
-    if (!email || !password) {
+    if (!credential) {
       toast({
         description: "Input field cannot be empty",
       })
       return
     }
 
-    try {
-      const { data } = await axios.post('https://coinstore-backend.onrender.com/api/user/login', {
-        email,
-        password,
-        role: "BUYER",
+    if(!validatePhoneNumber(credential)){
+      toast({
+        description: "Please enter 10 digit phone number"
       })
-      if (data.success) {
-        localStorage.setItem("token", data.token)
-        localStorage.setItem("user", JSON.stringify(data.user))
+    }
+
+    try {
+      const {data: otpVerification} = await axios.post('https://coinstore-backend.onrender.com/api/user/verifyOtp',{
+        credential,
+        otp: value
+      })
+      // console.log(otpVerification);
+      
+      if(otpVerification.success){
         toast({
-          description: "Logged In successfully",
+          description: otpVerification.message
         })
-        redirectURL ? navigate(`/checkout/${redirectURL}`) : navigate('/')
-      } else {
+        setOpen(false)
+        // console.log(credential);
+        const { data } = await axios.post('https://coinstore-backend.onrender.com/api/user/login', {
+          phone: credential,
+          // password,
+          role: "BUYER",
+        })
+        if (data.success) {
+          localStorage.setItem("token", data.token)
+          localStorage.setItem("user", JSON.stringify(data.user))
+          toast({
+            description: "Logged In successfully",
+          })
+          redirectURL ? navigate(`/checkout/${redirectURL}`) : navigate('/')
+        } else {
+          toast({
+            description: data.message,
+          })
+          // console.log(data);
+        }
+      }else{
         toast({
-          description: "Internal Server Error",
+          description: otpVerification.message
         })
-        console.log(data);
       }
     } catch (error) {
       localStorage.removeItem("token")
@@ -58,10 +82,88 @@ export default function SignIn() {
 
   }
 
-  function otpModal(e:any) {
-    setOpen(true)
+  async function otpModal(e:any) {
     e.preventDefault()
+    if (!credential) {
+      toast({
+        description: "Input field cannot be empty",
+      })
+      return
+    }
+
+    if(!validatePhoneNumber(credential)){
+      toast({
+        description: "Please enter 10 digit valid phone number"
+      })
+      return
+    }
+    try {
+      const {data} = await axios.post('https://coinstore-backend.onrender.com/api/user/sendOtp',{
+        credential
+      })
+      // console.log(data);
+      toast({
+        description: data.msg
+      })
+      setOpen(true)
+    } catch (error) {
+      toast({
+        description: (error as Error).message
+      })
+      setOpen(false)
+    }
   }
+
+  useEffect(()=>{
+    const user = localStorage.getItem('user')
+    const token = localStorage.getItem('token')
+    
+    if(user || token){
+      navigate('/')
+    }
+  },[])
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    if (isButtonDisabled && open) {
+      // console.log("hi");
+      
+      const countdown = setInterval(() => {
+        setTimer((prevTimer) => {
+          if (prevTimer === 1) {
+            clearInterval(countdown);
+            setIsButtonDisabled(false);
+            return 0;
+          }
+          return prevTimer - 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(countdown); // Cleanup interval on unmount
+    }
+  }, [isButtonDisabled, open]);
+
+  const handleResendOTP = async () => {
+    // Logic to resend the OTP
+    // console.log("Resending OTP...");
+    try {
+      const {data} = await axios.post('https://coinstore-backend.onrender.com/api/user/resendOtp',{
+        credential
+      })
+      console.log(data);
+      toast({
+        description: data.msg
+      })
+      setIsButtonDisabled(true);
+      setTimer(60); // Reset the timer
+    } catch (error) {
+      toast({
+        description: (error as Error).message
+      })
+    }
+  };
 
   return (
     <section className="bg-white font-PostJb">
@@ -91,16 +193,16 @@ export default function SignIn() {
 
 
               <div className="col-span-6">
-                <label htmlFor="Email" className="block text-sm font-medium text-gray-700"> Email </label>
+                <label htmlFor="Email" className="block text-sm font-medium text-gray-700"> Phone </label>
 
-                <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e) => setEmail(e.target.value)} />
+                <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e) => setCredential(e.target.value)} />
               </div>
 
-              <div className="col-span-6">
+              {/* <div className="col-span-6">
                 <label htmlFor="Password" className="block text-sm font-medium text-gray-700"> Password </label>
 
                 <Input type="password" id="pass" className="w-[100%] text-black" onChange={(e) => setPassword(e.target.value)} />
-              </div>
+              </div> */}
 
 
               <div className="col-span-6">
@@ -108,7 +210,7 @@ export default function SignIn() {
                   className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-lg font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 w-full"
                   onClick={otpModal}
                 >
-                  Login
+                  Login with OTP
                 </Button>
                 <Dialog open={open} onOpenChange={()=>setOpen(false)} >
                   <DialogContent style={{height:'fit-content'}}>
@@ -117,7 +219,8 @@ export default function SignIn() {
                     Mobile Phone Verification
                     </DialogTitle>
                     <DialogDescription className="font-PostSB text-[16px]">
-                    Enter the 6-digit verification code that was sent to your phone number.
+                      Enter the 6-digit verification code that was sent to your phone number.
+                      The code is valid for 1 hour
                     </DialogDescription>
                     </DialogHeader>
                   <InputOTP maxLength={6} onChange={(e)=>setValue(e)} pattern={REGEXP_ONLY_DIGITS}>
@@ -131,7 +234,8 @@ export default function SignIn() {
                     </InputOTPGroup>
                   </InputOTP>
                   <DialogFooter>
-                    <Button  className="m-0 font-bold bg-cyan-600 hover:bg-cyan-800 text-white" >Submit</Button>
+                    <Button  className="m-0 font-bold bg-cyan-600 hover:bg-cyan-800 text-white" onClick={validateUser}>Submit</Button>
+                    <Button onClick={handleResendOTP} disabled={isButtonDisabled} variant={'ghost'}>{isButtonDisabled ? `Resend OTP in ${timer}s` : "Resend OTP"}</Button>
                   </DialogFooter>
                   </DialogContent>
                 </Dialog>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,92 +2,199 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { useToast } from "@/hooks/use-toast";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
 import { REGEXP_ONLY_DIGITS } from "input-otp";
+import validatePhoneNumber from "@/utils/validatePhone";
+import validateEmail from "@/utils/validateEmail";
 
+export default function SignUp() {
 
-export default function SignUp(){
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  // const [pwd, setPwd] = useState('')
+  // const [cnfPwd, setCnfPwd] = useState('')
+  const [number, setNumber] = useState('')
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
 
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [pwd,setPwd] = useState('')
-    const [cnfPwd,setCnfPwd] = useState('')
-    const [number,setNumber] = useState('')
-    const [open, setOpen] = useState(false)
-    const [value, setValue] = useState("")
+  const navigate = useNavigate()
+  const { toast } = useToast()
 
-    const navigate = useNavigate()
-    const {toast} = useToast()
-
-    async function authUser(e:any) {
-        e.preventDefault()
-        if(!name || !email || !pwd || !cnfPwd || !number ){
-          toast({
-            description: "Input field cannot be empty",
-          })
-          return 
-        }
-        if(number.length !== 10 ){
-          toast({
-            description: "Enter valid phone number",
-          })
-          return 
-        }
-        if(pwd !== cnfPwd){
-          toast({
-            description: "Password and Confirm Password not matching",
-          })
-          return
-        }
-        try {
-          const {data} = await axios.post('https://coinstore-backend.onrender.com/api/user/signup', {
-            username:name,
-            email,
-            password: pwd,
-            phone:number,
-            role: "BUYER",
-          })
-          if(data.success){
-            localStorage.setItem("token", data.token)
-            localStorage.setItem("user", JSON.stringify(data.user))
-            toast({
-              description: "User created successfully",
-            })
-            navigate('/')
-          } else{
-            toast({
-              description: data.message,
-            })
-            console.log(data);
-          }
-        } catch (error) {
-          localStorage.removeItem("token")
-          localStorage.removeItem("user")
-          console.log(error);
-          toast({
-            description: "Internal Server Error",
-          })
-        }
-
+  async function authUser(e: any) {
+    e.preventDefault()
+    if (!name || !email || !number) {
+      toast({
+        description: "Input field cannot be empty",
+      })
+      return
     }
-    function otpModal(e:any) {
+    if(!validateEmail(email)){
+      toast({
+        description: "Please enter a valid email"
+      })
+      return
+    }
+    if (!validatePhoneNumber(number)) {
+      toast({
+        description: "Enter valid phone number",
+      })
+      return
+    }
+    // if (pwd !== cnfPwd) {
+    //   toast({
+    //     description: "Password and Confirm Password not matching",
+    //   })
+    //   return
+    // }
+    try {
+      const {data: otpVerification} = await axios.post('https://coinstore-backend.onrender.com/api/user/verifyOtp',{
+        credential: number,
+        otp: value
+      })
+      if(otpVerification.success){
+        toast({
+          description: otpVerification.message
+        })
+        setOpen(false)
+
+        const { data } = await axios.post('https://coinstore-backend.onrender.com/api/user/signup', {
+          username: name,
+          email,
+          // password: pwd,
+          phone: number,
+          role: "BUYER",
+        })
+        if (data.success) {
+          localStorage.setItem("token", data.token)
+          localStorage.setItem("user", JSON.stringify(data.user))
+          toast({
+            description: "User created successfully",
+          })
+          navigate('/')
+        } else {
+          toast({
+            description: data.message,
+          })
+          console.log(data);
+        }
+      }else {
+        toast({
+          description: otpVerification.message
+        })
+      }
+    } catch (error) {
+      localStorage.removeItem("token")
+      localStorage.removeItem("user")
+      console.log(error);
+      toast({
+        description: "Internal Server Error",
+      })
+    }
+
+  }
+  async function otpModal(e:any) {
+    e.preventDefault()
+    if (!number || !name || !email) {
+      toast({
+        description: "Input field cannot be empty",
+      })
+      return
+    }
+    
+    if(!validatePhoneNumber(number)){
+      toast({
+        description: "Please enter 10 digit valid phone number"
+      })
+      return
+    }
+    
+    if(!validateEmail(email)){
+      toast({
+        description: "Please enter a valid email"
+      })
+      return
+    }
+    try {
+      const {data} = await axios.post('https://coinstore-backend.onrender.com/api/user/sendOtp',{
+        credential: number
+      })
+      // console.log(data);
       setOpen(true)
-      e.preventDefault()
+      toast({
+        description: data.msg
+      })
+    } catch (error) {
+      toast({
+        description: (error as Error).message
+      })
+      setOpen(false)
     }
+  }
 
-    return(
-<section className="bg-white font-PostJb">
-  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-    <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-      <img
-        alt=""
-        src="https://images.unsplash.com/photo-1529154691717-3306083d869e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        className="absolute inset-0 h-full w-full object-cover"
-      />
-    </aside>
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+    const token = localStorage.getItem('token')
+
+    if (user || token) {
+      navigate('/')
+    }
+  }, [])
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    if (isButtonDisabled && open) {
+      // console.log("hi");
+      
+      const countdown = setInterval(() => {
+        setTimer((prevTimer) => {
+          if (prevTimer === 1) {
+            clearInterval(countdown);
+            setIsButtonDisabled(false);
+            return 0;
+          }
+          return prevTimer - 1;
+        });
+      }, 1000);
+
+      return () => clearInterval(countdown); // Cleanup interval on unmount
+    }
+  }, [isButtonDisabled, open]);
+
+  const handleResendOTP = async () => {
+    // Logic to resend the OTP
+    // console.log("Resending OTP...");
+    try {
+      const {data} = await axios.post('https://coinstore-backend.onrender.com/api/user/resendOtp',{
+        credential: number
+      })
+      console.log(data);
+      toast({
+        description: data.msg
+      })
+      setIsButtonDisabled(true);
+      setTimer(60); // Reset the timer
+    } catch (error) {
+      toast({
+        description: (error as Error).message
+      })
+    }
+  }
+
+  return (
+    <section className="bg-white font-PostJb">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="https://images.unsplash.com/photo-1529154691717-3306083d869e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+        </aside>
 
         <main
           className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
@@ -98,87 +205,88 @@ export default function SignUp(){
               Welcome to Shadow Company
             </h1>
 
-        <p className="mt-4 leading-relaxed text-gray-500">
-        Embark on your ultimate gaming journey! Sign up now to unlock exclusive content, connect with players worldwide, and dive into endless adventures.
-        </p>
+            <p className="mt-4 leading-relaxed text-gray-500">
+              Embark on your ultimate gaming journey! Sign up now to unlock exclusive content, connect with players worldwide, and dive into endless adventures.
+            </p>
 
-        <form className="mt-8 grid grid-cols-6 gap-6">
-          <div className="col-span-6">
-            <label htmlFor="FirstName" className="block text-sm font-medium text-gray-700">
-              Name
-            </label>
+            <form className="mt-8 grid grid-cols-6 gap-6">
+              <div className="col-span-6">
+                <label htmlFor="FirstName" className="block text-sm font-medium text-gray-700">
+                  Name
+                </label>
 
-            <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e)=>setName(e.target.value)}/>
-          </div>
+                <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e) => setName(e.target.value)} />
+              </div>
 
               <div className="col-span-6">
                 <label htmlFor="Email" className="block text-sm font-medium text-gray-700"> Email </label>
 
-            <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e)=>setEmail(e.target.value)} />
-          </div>
+                <Input type="text" id="userid" className="w-[100%] text-black" onChange={(e) => setEmail(e.target.value)} />
+              </div>
 
-          <div className="col-span-6">
-            <label htmlFor="Phone Number" className="block text-sm font-medium text-gray-700"> Phone Number </label>
+              <div className="col-span-6">
+                <label htmlFor="Phone Number" className="block text-sm font-medium text-gray-700"> Phone Number </label>
 
-            <Input type="number" id="userid" className="w-[100%] text-black" onChange={(e)=>setNumber(e.target.value)} />
-          </div>
+                <Input type="number" id="userid" className="w-[100%] text-black" onChange={(e) => setNumber(e.target.value)} />
+              </div>
 
-          <div className="col-span-6">
-            <label htmlFor="Password" className="block text-sm font-medium text-gray-700"> Password </label>
+              {/* <div className="col-span-6">
+                <label htmlFor="Password" className="block text-sm font-medium text-gray-700"> Password </label>
 
-            <Input type="password" id="userid" className="w-[100%] text-black" onChange={(e)=>setPwd(e.target.value)} />
-          </div>
+                <Input type="password" id="userid" className="w-[100%] text-black" onChange={(e) => setPwd(e.target.value)} />
+              </div>
 
-          <div className="col-span-6 ">
-            <label htmlFor="PasswordConfirmation" className="block text-sm font-medium text-gray-700">
-              Confirm Password
-            </label>
+              <div className="col-span-6 ">
+                <label htmlFor="PasswordConfirmation" className="block text-sm font-medium text-gray-700">
+                  Confirm Password
+                </label>
 
-            <Input type="password" id="userid" className="w-[100%] text-black" onChange={(e)=>setCnfPwd(e.target.value)} />
-          </div>
+                <Input type="password" id="userid" className="w-[100%] text-black" onChange={(e) => setCnfPwd(e.target.value)} />
+              </div> */}
 
-          <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
-          <Button
-              className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-lg font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
-              onClick={otpModal}
-            >
-              Create an account
-            </Button>
-            <Dialog open={open} onOpenChange={()=>setOpen(false)} >
-                  <DialogContent style={{height:'fit-content'}}>
+              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                <Button
+                  className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-lg font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                  onClick={otpModal}
+                >
+                  Verify using OTP
+                </Button>
+                <Dialog open={open} onOpenChange={() => setOpen(false)} >
+                  <DialogContent style={{ height: 'fit-content' }}>
                     <DialogHeader>
-                    <DialogTitle className="font-PostSB text-[20px]">
-                    Mobile Phone Verification
-                    </DialogTitle>
-                    <DialogDescription className="font-PostSB text-[16px]">
-                    Enter the 6-digit verification code that was sent to your phone number.
-                    </DialogDescription>
+                      <DialogTitle className="font-PostSB text-[20px]">
+                        Mobile Phone Verification
+                      </DialogTitle>
+                      <DialogDescription className="font-PostSB text-[16px]">
+                        Enter the 6-digit verification code that was sent to your phone number.
+                      </DialogDescription>
                     </DialogHeader>
-                  <InputOTP maxLength={6} onChange={(e)=>setValue(e)} pattern={REGEXP_ONLY_DIGITS}>
-                    <InputOTPGroup>
-                      <InputOTPSlot index={0} />
-                      <InputOTPSlot index={1} />
-                      <InputOTPSlot index={2} />
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                      <InputOTPSlot index={5} />
-                    </InputOTPGroup>
-                  </InputOTP>
-                  <DialogFooter>
-                    <Button  className="m-0 font-bold bg-cyan-600 hover:bg-cyan-800 text-white" >Submit</Button>
-                  </DialogFooter>
+                    <InputOTP maxLength={6} onChange={(e) => setValue(e)} pattern={REGEXP_ONLY_DIGITS}>
+                      <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                        <InputOTPSlot index={2} />
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                        <InputOTPSlot index={5} />
+                      </InputOTPGroup>
+                    </InputOTP>
+                    <DialogFooter>
+                      <Button className="m-0 font-bold bg-cyan-600 hover:bg-cyan-800 text-white" onClick={authUser}>Submit</Button>
+                      <Button onClick={handleResendOTP} disabled={isButtonDisabled} variant={'ghost'}>{isButtonDisabled ? `Resend OTP in ${timer}s` : "Resend OTP"}</Button>
+                    </DialogFooter>
                   </DialogContent>
                 </Dialog>
 
-            <p className="mt-4 text-sm text-gray-500 sm:mt-0">
-              Already have an account?
-              <a href="/login" className="text-gray-700 underline">Log in</a>.
-            </p>
+                <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                  Already have an account?
+                  <a href="/login" className="text-gray-700 underline">Log in</a>.
+                </p>
+              </div>
+            </form>
           </div>
-        </form>
+        </main>
       </div>
-    </main>
-  </div>
-</section>
-    )
+    </section>
+  )
 }
